e3ed58e9e7be0abbed2cae18195c476af40f1e96
JayMaisuriya <104067329+JayMaisuriya@users.noreply.github.com>
2022-12-20 11:59:04 +0530

    Add files via upload

--- pr10_3.py
+++ pr10_3.py
@@ -1,5 +1,9 @@
 import datetime
-today=datetime.date.today()
+y=int(input("ENTER YEAR: "))
+m=int(input("ENTER MONTH: "))
+d=int(input("ENTER DAY: "))
+
+today=datetime.date(y,m,d)
 print(today)
 formatdate=datetime.date.strftime(today,"%m-%d-%Y")
 print(formatdate,"\n")
---
+++ pr10_4.py
@@ -0,0 +1,6 @@
+a=(str(input("\nENTER FIRST WORD: ")))
+b=(str(input("ENTER FIRST WORD: ")))
+if (sorted(a)==sorted(b)):
+    print("GIVEN WORD IS ANAGRAMS\n")
+else:
+    print("GIVEN WORD IS NOT ANAGRAMS\n") 

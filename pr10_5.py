de9f8cb27408f19eac1679b8225e048156d63f81
JayMaisuriya <104067329+JayMaisuriya@users.noreply.github.com>
2022-12-20 12:32:59 +0530

    Add files via upload

---
+++ pr10_5.py
@@ -0,0 +1,8 @@
+def colorcode(hex):
+    rgb=[]
+    for i in (0,2,4):
+        decimal=int(hex[i:i+2],16)
+        rgb.append(decimal)
+    return tuple(rgb)
+code=input("ENTER COLOR CODE: ")
+print(colorcode(code))
